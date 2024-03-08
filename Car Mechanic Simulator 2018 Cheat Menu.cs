010eb4b7173b76532ce110158fcbafd42e3e4854
01KikoSK <59251184+01KikoSK@users.noreply.github.com>
2024-03-08 14:44:46 +0100

    Add files via upload

---
+++ Car Mechanic Simulator 2018 Cheat Menu.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Windows.Forms;
+
+namespace CarMechanicSimulator2018CheatMenu
+{
+    public partial class CheatMenuForm : Form
+    {
+        public CheatMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void setPartsQualityToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Code to set parts quality goes here
+        }
+
+        private void revealConditionOfAllPartsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Code to reveal condition of all parts goes here
+        }
+
+        private void alwaysUnmountableToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Code to make all parts always unmountable goes here
+        }
+
+        private void fastUnmountToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Code to implement fast unmount goes here
+        }
+
+        private void perfectInventoryItemsConditionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Code to multiply inventory item condition goes here
+        }
+    }
+}
